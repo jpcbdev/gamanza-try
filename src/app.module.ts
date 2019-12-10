0f8305042f5d702da8f6e4b45674c9b5ed9e3bed
lixo <j>
2019-12-10 03:44:27 -0600

    mod: mongoose config params modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,19 @@ import { VeterinaryController } from './veterinary/veterinary.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
-    MongooseModule.forFeature([
-      { name: 'Veterinary', schema: VeterinarySchema, collection: 'veterinary' }
-    ])
+    MongooseModule.forRoot('mongodb://localhost/veterinary', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      autoIndex: false,
+      poolSize: 10,
+      bufferMaxEntries: 0,
+      connectTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+      family: 4
+    }),
+    MongooseModule.forFeature([{ name: 'Veterinary', schema: VeterinarySchema, collection: 'veterinary' }])
   ],
   controllers: [AppController, VeterinaryController],
   providers: [AppService, VeterinaryService]
